b7591a65da7537eae727f1b81ec840ad26413645
admin <admin@admin-49.local>
2015-04-02 17:31:56 -0400

    Initial Commit

---
+++ sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sample
+//
+//  Created by admin on 4/2/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sample
+//
+//  Created by admin on 4/2/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
